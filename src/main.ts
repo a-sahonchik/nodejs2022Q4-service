4ade0a12137ef0865490d1dff66af3a892d498cb
Alexey <sahonchik.alexey@gmail.com>
2023-02-02 09:54:29 +0300

    feat: parse PORT value from env

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
+import * as dotenv from 'dotenv';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+
+dotenv.config();
+
+const port = parseInt(process.env['PORT'], 10);
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,6 +18,7 @@ async function bootstrap() {
     }),
   );
 
-  await app.listen(4000);
+  await app.listen(port);
 }
+
 bootstrap();
